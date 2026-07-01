e64a4c6927c6075945503f8abd9588225b832f9f
NTPCMD <newtownpunjabi@gmail.com>
2026-07-01 06:46:07 +0000

    Use public socket URL for production

--- examples/websocket/frontend.tsx
+++ examples/websocket/frontend.tsx
@@ -33,14 +33,12 @@ export default function SocketDemo() {
     // Connect to websocket server
     // Never use PORT in the URL, alyways use XTransformPort
     // DO NOT change the path, it is used by Caddy to forward the request to the correct port
-    const socketInstance = io('/?XTransformPort=3003', {
-      transports: ['websocket', 'polling'],
-      forceNew: true,
-      reconnection: true,
-      reconnectionAttempts: 5,
-      reconnectionDelay: 1000,
-      timeout: 10000
-    })
+  const socketInstance = io(
+  process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3003",
+  {
+    transports: ["websocket", "polling"],
+  }
+);
 
     setSocket(socketInstance);
 
